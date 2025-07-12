140f02114d5ff17cb07f3e1425b5187c447bfa22
Krystian Kurlej <235661@student.uek.krakow.pl>
2025-07-12 15:03:29 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿while (true)
+{
+    // Console.Clear();
+    Console.WriteLine("1. Wyświetl uczestników");
+    Console.WriteLine("2. Dodaj uczestnika");
+    Console.WriteLine("3. Usuń uczestnika");
+    Console.WriteLine("4. Wygeneruj arkusz obecności");
+    Console.WriteLine("Wpisz numer pozycji, która Cię interesuje:");
+
+    string choice = Console.ReadLine();
+
+    Console.WriteLine(choice);
+}
